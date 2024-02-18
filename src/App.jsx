74f5430e4cf995c0cc1171c70ef223775ee692fb
Vilma Hernandez <vilmaglynn@hotmail.com>
2024-02-18 14:46:28 +0000

    initial home component

--- src/App.jsx
+++ src/App.jsx
@@ -14,15 +14,15 @@ const App = () => {
     <Router>
       <div className={styles.App}>
         <Header />
-      </div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/experience" element={<Experience />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
 
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,21 @@
 
 import React from 'react';
 
+import styles from "./Home.module.css"
+import { getImageUrl } from "../../utils"
+
 const Home = () => {
 	return (
-		<div>
-			<h1>Home</h1>
-			<p>Welcome to the home page!</p>
-		</div>
+		<section className={styles.container}>
+			<div className={styles.content}>
+				<h1 className={styles.title}>Hello, I'm Vilma</h1>
+				<p className={styles.description}>Frontend Software Developer: A Javascript and React enthusiast 🔆</p>
+				<a href="mailto:@gmail.com" className={styles.contactBtn}></a>
+			</div>
+			<img src={getImageUrl("Avatar2.png")} alt="" className={styles.heroImg} />
+			<div className={styles.topBlur}></div>
+			<div className={styles.bottomBlur}></div>
+		</section>
 	);
 }
 
