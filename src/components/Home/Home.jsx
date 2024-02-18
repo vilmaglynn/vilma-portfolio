112c1f13652a942abbf5f9ff527b2f6c0dd7427a
Vilma Hernandez <vilmaglynn@hotmail.com>
2024-02-18 15:56:21 +0000

    changes to hero section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,18 +4,39 @@ import React from 'react';
 import styles from "./Home.module.css"
 import { getImageUrl } from "../../utils"
 
+// const Home = () => {
+// 	return (
+// 		<section className={styles.container}>
+// 			<div className={styles.content}>
+// 				<h1 className={styles.title}>Hello, I'm Vilma</h1>
+// 				<p className={styles.description}>Frontend Software Developer: A Javascript and React enthusiast 🔆</p>
+// 				<a href="mailto:@gmail.com" className={styles.contactBtn}></a>
+// 			</div>
+// 			<img src={getImageUrl("Avatar2.png")} alt="" className={styles.heroImg} />
+// 			<div className={styles.topBlur}></div>
+// 			<div className={styles.bottomBlur}></div>
+// 		</section>
+// 	);
+// }
+
 const Home = () => {
 	return (
-		<section className={styles.container}>
-			<div className={styles.content}>
-				<h1 className={styles.title}>Hello, I'm Vilma</h1>
-				<p className={styles.description}>Frontend Software Developer: A Javascript and React enthusiast 🔆</p>
-				<a href="mailto:@gmail.com" className={styles.contactBtn}></a>
+		<div className={`hero-section ${styles.container}`}>
+			<div className="row align-items-center">
+				{/* Text on the left */}
+				<div className="col-md-6">
+					<div >
+						<h1 className={styles.title}>Hello, I'm Vilma</h1>
+						<p className={styles.description}>Frontend Software Developer: a Javascript and React enthusiast 🔆</p>
+						<a href="mailto:@gmail.com" className={styles.contactBtn}></a>
+					</div>
+				</div>
+				{/* Image on the right */}
+				<div className="col-md-6">
+					<img src={getImageUrl("Avatar2.png")} alt="Hero Image" className={`img-fluid hero-image rounded-circle ${styles.heroImg}`} />
+				</div>
 			</div>
-			<img src={getImageUrl("Avatar2.png")} alt="" className={styles.heroImg} />
-			<div className={styles.topBlur}></div>
-			<div className={styles.bottomBlur}></div>
-		</section>
+		</div>
 	);
 }
 
