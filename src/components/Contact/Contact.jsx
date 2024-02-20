818fbeca767110e2d74820f55a3b5e737c3cd0a3
Vilma Hernandez <vilmaglynn@hotmail.com>
2024-02-20 21:54:24 +0000

    amended contact a bit more

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -16,8 +16,6 @@ const Contact = () => {
 						<h2>Contact</h2>
 						<p>Feel free to reach out!</p>
 					</div>
-				</div>
-				<div className="col-lg-6">
 					<ul className={styles.links}>
 						<li className={styles.link}>
 							<FontAwesomeIcon icon="fa-solid fa-envelope" className={styles.icon} />
@@ -31,29 +29,37 @@ const Contact = () => {
 							<FontAwesomeIcon icon="fa-brands fa-github" className={styles.icon} />
 							<a href="https://www.github.com/myname">github.com/myname</a>
 						</li>
+						<li className={styles.link}>
+							<FontAwesomeIcon icon="fa-solid fa-phone" className={styles.icon} />
+							<a href="tel:+1234567890">123-456-7890</a>
+						</li>
+						<li className={styles.link}>
+							<FontAwesomeIcon icon="fa-solid fa-file-pdf" className={styles.icon} />
+							<a href="/path/to/resume.pdf" download>Download Resume</a>
+						</li>
 					</ul>
 				</div>
-			</div>
-			<hr>
-			</hr>
-			<div className="row">
-				<div className="col-lg-12 mx-auto" style={{ maxWidth: '500px' }}>
-					<h2 className="mt-5">Send me a message</h2>
-					<form >
-						<div className="mb-3">
-							<label htmlFor="name" className="form-label">Name:</label>
-							<input type="text" className="form-control" id="name" name="name" required />
+				<div className="col-lg-6">
+					<div className="row">
+						<div className="col-lg-12 mx-auto" style={{ maxWidth: '500px' }}>
+							<h2 className="mt-5">Send me a message</h2>
+							<form>
+								<div className="mb-3">
+									<label htmlFor="name" className="form-label">Name:</label>
+									<input type="text" className="form-control" id="name" name="name" required />
+								</div>
+								<div className="mb-3">
+									<label htmlFor="email" className="form-label">Email:</label>
+									<input type="email" className="form-control" id="email" name="email" required />
+								</div>
+								<div className="mb-3">
+									<label htmlFor="message" className="form-label">Message:</label>
+									<textarea className="form-control" id="message" name="message" rows="4" required></textarea>
+								</div>
+								<button type="submit" className={`btn btn-primary ${styles.submitButton}`}>Submit</button>
+							</form>
 						</div>
-						<div className="mb-3">
-							<label htmlFor="email" className="form-label">Email:</label>
-							<input type="email" className="form-control" id="email" name="email" required />
-						</div>
-						<div className="mb-3">
-							<label htmlFor="message" className="form-label">Message:</label>
-							<textarea className="form-control" id="message" name="message" rows="4" required></textarea>
-						</div>
-						<button type="submit" className={`btn btn-primary ${styles.submitButton}`}>Submit</button>
-					</form>
+					</div>
 				</div>
 			</div>
 		</section>
